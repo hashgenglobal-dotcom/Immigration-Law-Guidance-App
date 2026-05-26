07228be83b3e344afed95f23bdbb97002893740a
Hash <hash@hashgen.global>
2026-05-26 17:43:45 -0400

    fix: resolve merge conflicts in routing and navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import ChatPage from './pages/ChatPage'
-<<<<<<< HEAD
-=======
 import ScenariosPage from './pages/ScenariosPage'
->>>>>>> features/more-guide
 import OfficialUpdatesPage from './pages/OfficialUpdatesPage'
 import SourcesPage from './pages/SourcesPage'
 import AboutPage from './pages/AboutPage'
@@ -15,10 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/chat" element={<ChatPage />} />
-<<<<<<< HEAD
-=======
         <Route path="/scenarios" element={<ScenariosPage />} />
->>>>>>> features/more-guide
         <Route path="/updates" element={<OfficialUpdatesPage />} />
         <Route path="/sources" element={<SourcesPage />} />
         <Route path="/about" element={<AboutPage />} />
